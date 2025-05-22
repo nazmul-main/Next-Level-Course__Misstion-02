ceb3bb2f1fde9aced8b1229fbb95349e6bc921ba
nazmulhossen1 <nazmulhossen.main@gmail.com>
2025-05-22 16:57:58 +0600

    added a new note on this topic : Event driven architecture, create your own events

--- Module - 7/lerning-node/event-emitter.js
+++ Module - 7/lerning-node/event-emitter.js
@@ -1,3 +1,13 @@
-const EeventEmitter = require('events');
+const EventEmitter = require("events");
 
+const myEmitter = new EventEmitter();
 
+myEmitter.on("birthday", () => {
+  console.log("Happy Birthday To You");
+});
+
+myEmitter.on("birthday", (gift) => {
+  console.log(`I will send a gift that is: ${gift}`);
+});
+
+myEmitter.emit("birthday", "Watch");
